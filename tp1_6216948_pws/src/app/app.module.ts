812505936da82c379a9f0bcd15de0f10ddf50e07
Jordan Couture-Lafranchise <6216948@cegepmontpetit.ca>
2023-09-24 22:59:30 -0400

    i18n,css
    traduction avec i18n, ajout d'un peu de css aux boutons

--- tp1_6216948_pws/src/app/app.module.ts
+++ tp1_6216948_pws/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { SaisonsComponent } from './saisons/saisons.component';
 import { RouterModule } from '@angular/router';
 import { LeaguesComponent } from './leagues/leagues.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 
 @NgModule({
@@ -27,9 +29,21 @@ import { LeaguesComponent } from './leagues/leagues.component';
       { path: 'ligues', component: LeaguesComponent },
       { path: 'saisons/:leagueName', component: SaisonsComponent },
       { path: 'classements/:leagueId/:Annee', component: RankingsComponent },
-  ])
+  ]),
+
+  TranslateModule.forRoot({
+    loader:{
+      provide:TranslateLoader,
+      useFactory:HttpLoaderFactory,
+      deps:[HttpClient]
+    }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http:HttpClient){
+  return new TranslateHttpLoader(http);
+}
